69b3e98f7b11bedc9d7eb7168a696814df6bfa00
eugenchio <eugenchio@gmail.com>
2024-12-19 16:58:39 +0200

    Update photoswipe lightbox to use esm version of code.

--- static/js/lightbox.js
+++ static/js/lightbox.js
@@ -1,4 +1,13 @@
+import PhotoSwipeLightbox from '/vendor/photoswipe-lightbox.esm.min.js';
+import PhotoSwipe from '/vendor/photoswipe.esm.min.js';
+
 window.addEventListener("load", function () {
+  const link = document.createElement('link');
+  link.type = 'text/css';
+  link.rel = 'stylesheet';
+  document.head.appendChild(link);
+
+  link.href = '/vendor/photoswipe.css';
   document.querySelectorAll('img').forEach(function (img) {
     if (img.parentNode.tagName === "A") {
       img.parentNode.setAttribute("data-pswp-width", img.naturalWidth);
