0f1774d5f59b60f68a177193fe5d54a32e67edf8
eugenchio <eugenchio@gmail.com>
2024-12-20 11:40:24 +0200

    Remove unnecessary class of h2 anchor links.

--- static/js/prettify.js
+++ static/js/prettify.js
@@ -8,7 +8,7 @@ function activateAnchors() {
   document.querySelectorAll('h2').forEach(function (h) {
     var id = h.id;
     if (id) {
-      h.innerHTML = '<a href="#' + id + '" class="anchor">' + h.innerHTML + '</a>';
+      h.innerHTML = '<a href="#' + id + '">' + h.innerHTML + '</a>';
     }
   });
 }
